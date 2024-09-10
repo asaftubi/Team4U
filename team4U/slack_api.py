e9068cb85a94034551b03565c22ca6fbc90bac5c
asaftubi <asaftuby@gmail.com>
2024-09-10 15:03:49 +0300

    change token

--- team4U/slack_api.py
+++ team4U/slack_api.py
@@ -11,8 +11,8 @@ load_dotenv(dotenv_path='.env')  # וודא שהקובץ נטען
 SLACK_BOT_TOKEN = os.getenv('SLACK_BOT_TOKEN')
 CHANNEL_ID = os.getenv('CHANNEL_ID')
 
-
-print("sasaa")
+print(SLACK_BOT_TOKEN)
+print(CHANNEL_ID)
 print("hello team4U")
 
 # Create a WebClient object with your token
